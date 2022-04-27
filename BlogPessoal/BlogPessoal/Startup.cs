ccc7f98eb567a45423d3a536ad8863d80eace552
YasmimCristine <yasmim-vgs@outlook.com>
2022-04-27 17:12:21 -0300

    @YasmimCristine [Task: ADD dependencias] {27/04/2022 17:15}

--- BlogPessoal/BlogPessoal/Startup.cs
+++ BlogPessoal/BlogPessoal/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -16,6 +17,11 @@ namespace BlogPessoal
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            IConfigurationRoot config = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile("appsettings.json")
+                .Build();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
